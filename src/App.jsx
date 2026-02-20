525e87e93debf0ac7d77aecb00ccb62de65cc250
Bipin Ghimire <bipinghimire265@gmail.com>
2026-02-20 17:33:03 +0545

    Update Basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ScrollTrigger, SplitText} from 'gsap/all';
 import Navbar from './components/Navbar'; 
+import Hero from './components/Hero'; 
 import gsap from 'gsap';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
     
   )
