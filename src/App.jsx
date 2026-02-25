79b29c4d7be98b53353a72fa692f57174fc0eda0
Bipin Ghimire <bipinghimire265@gmail.com>
2026-02-25 16:43:19 +0545

    Some Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {ScrollTrigger, SplitText} from 'gsap/all';
 import Navbar from './components/Navbar'; 
 import Hero from './components/Hero'; 
 import gsap from 'gsap';
+import Cocktails from './components/Cocktails';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -11,6 +12,8 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
+     
     </main>
     
   )
