ccfa05f511c2253763d7461d9a53b9c1eaa10a4b
Bipin Ghimire <bipinghimire265@gmail.com>
2026-03-09 23:45:19 +0545

    Implement Menu Section

---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { allCocktails } from '../../constants/index.js'
+
+
+const Menu = () => {
+
+  return (
+   <section id="menu" aria-labelledby="menu-heading">
+	 <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf" />
+	 <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf" />
+	 
+	 <h2 id="menu-heading" className="sr-only">
+		Cocktail Menu
+	 </h2>
+	 
+	
+	</section>
+  )
+}
+
+export default Menu
