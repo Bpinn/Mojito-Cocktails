11677b65ef1ca7625da06a1709a6556697afe3b4
Bipin Ghimire <bipinghimire265@gmail.com>
2026-06-17 19:21:56 +0545

    Update Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
 import { openingHours, socials } from '../../constants/index.js'
+import { useGSAP } from '@gsap/react'
+import { SplitText} from 'gsap/all';
+import gsap from 'gsap';
 
 const Contact = () => {
-  return (
-    <footer id="contact">
+ 	useGSAP(() => {
+		const titleSplit = SplitText.create('#contact h2', { type: 'words' });
+		
+		const timeline = gsap.timeline({
+		 scrollTrigger: {
+			trigger: '#contact',
+			start: 'top center',
+		 },
+		 ease: "power1.inOut"
+		})
+	 
+	 timeline
+		.from(titleSplit.words, {
+		 opacity: 0, yPercent: 100, stagger: 0.02
+	 })
+		.from('#contact h3, #contact p', {
+			opacity: 0, yPercent: 100, stagger: 0.02
+	 })
+		.to('#f-right-leaf', {
+		 y: '-50', duration: 1, ease: 'power1.inOut'
+	 }).to('#f-left-leaf', {
+		 y: '-50', duration: 1, ease: 'power1.inOut'
+	 }, '<')
+	})
+ 
+ return (
+	<footer id="contact">
 	 <img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf" />
 	 <img src="/images/footer-left-leaf.png" alt="leaf-left" id="f-left-leaf" />
 	 
@@ -49,8 +76,7 @@ const Contact = () => {
 		</div>
 	 </div>
 	</footer>
- 
-  )
+ )
 }
 
 export default Contact
